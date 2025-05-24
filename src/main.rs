47ff865d0bc3218d6f47d9b7fd2327d45195dafe
chris flinn <christopher.flinn@gmail.com>
2025-05-25 08:43:23 +1000

    update

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use std::env;
-use std::fs;
-use std::io::{self, Write};
-use std::path::{Path, PathBuf};
-use std::process::{self, Command};
+use std::{
+    collections::HashMap,
+    env,
+    ffi::OsStr,
+    fs,
+    io::{self, Write},
+    path::{Path, PathBuf},
+    process::{self, Command},
+};
 
 fn exit(args: &[&str]) {
     let mut exit_status: i32 = args[0].parse().expect("exit argument not an int");
@@ -49,10 +53,15 @@ fn r#type(args: &[&str]) {
     }
 }
 
-fn runprogram(path: &Path, args: &[&str]) -> io::Result<()> {
-    let mut command = Command::new(path.file_name().expect("File name error"));
+fn run_program<S, I, P>(program: P, args: I) -> io::Result<()>
+where
+    S: AsRef<OsStr>,
+    I: IntoIterator<Item = S>,
+    P: AsRef<Path>,
+{
+    let mut command = Command::new(program.as_ref());
     command.args(args);
-    _ = command.spawn()?.wait()?;
+    command.spawn()?.wait()?;
     Ok(())
 }
 
@@ -62,7 +71,7 @@ fn pwd() -> io::Result<()> {
     Ok(())
 }
 fn cd(args: &[&str]) -> io::Result<()> {
-    let mut target = match args.first() {
+    let target = match args.first() {
         None | Some(&"~") => env::var_os("HOME").expect("Home dir error").into(),
         Some(s) if s.starts_with("~/") => {
             let mut home: PathBuf = env::var_os("HOME").expect("Home dir error 2").into();
@@ -83,6 +92,26 @@ fn cd(args: &[&str]) -> io::Result<()> {
 }
 
 fn main() -> io::Result<()> {
+    #[allow(clippy::type_complexity)]
+    let mut builtins: HashMap<&str, Box<dyn Fn(&[&str]) -> io::Result<()>>> = HashMap::new();
+    builtins.insert("cd", Box::new(cd));
+    builtins.insert("pwd", Box::new(|_| pwd()));
+    builtins.insert(
+        "echo",
+        Box::new(|args| {
+            echo(args);
+            Ok(())
+        }),
+    );
+    builtins.insert(
+        "exit",
+        Box::new(|args| {
+            exit(args);
+            #[allow(unreachable_code)]
+            Ok(())
+        }),
+    );
+
     loop {
         // prompt
         print!("$ ");
@@ -91,22 +120,20 @@ fn main() -> io::Result<()> {
         // user input
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
-        let cmd = input.trim();
         let v: Vec<&str> = input.split_whitespace().collect();
+        if !v.is_empty() {
+            continue;
+        }
         let command = v[0];
         let args: &[&str] = &v[1..];
 
         // command interpretation
-        match command {
-            "exit" => exit(args),
-            "echo" => echo(args),
-            "type" => r#type(args),
-            "pwd" => pwd()?,
-            "cd" => cd(args)?,
-            _ => match pathfind(command) {
-                Some(path) => runprogram(&path, args)?,
-                None => println!("{cmd}: command not found"),
-            },
+        if let Some(handler) = builtins.get(command) {
+            handler(args)?;
+        } else if let Some(path) = pathfind(command) {
+            run_program(path, args)?;
+        } else {
+            println!("{command}: command not found");
         }
     }
 }
