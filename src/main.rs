813842e31a3c85084741ef0afd8b084f19011868
chris flinn <christopher.flinn@gmail.com>
2025-05-24 10:00:17 +1000

    REPL

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+#[allow(unused_imports)]
+use std::io::{self, Write};
+
+fn main() {
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        // Wait for user input
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        println!("{}: command not found", input.trim());
+    }
+}
